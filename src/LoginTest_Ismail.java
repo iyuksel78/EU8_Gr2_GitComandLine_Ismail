7199c5e42fbffb843771268675662011c3e226fe
iyuksel78 <97805177+iyuksel78@users.noreply.github.com>
2022-04-28 22:01:30 +0300

    AC1 is completed

--- src/LoginTest_Ismail.java
+++ src/LoginTest_Ismail.java
@@ -1,5 +1,7 @@
 public class LoginTest_Ismail {
     public static void main(String[] args) {
         System.out.println("Login Class is created");
+
+        System.out.println("AC1 testing is PASSED");
     }
 }
